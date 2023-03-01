79195d2db3e016e2e6ed548a7cd7199768d3f7fd
SofianAcDev <sofian.achbabi@gmail.com>
2023-03-01 15:45:49 +0100

    finish animation

--- script.js
+++ script.js
@@ -10,26 +10,38 @@
 
 const popup = document.querySelector(".pop-up");
 
+let playOnce = true;
+
 window.addEventListener("scroll", () => {
   if (window.scrollY <= 25) {
     navbar.style.height = "90px";
   } else {
     navbar.style.height = "50px";
   }
-  if (window.scrollY > 280) {
+
+  let scrollValue =
+    (window.scrollY + window.innerHeight) / document.body.offsetHeight;
+
+  if (scrollValue > 0.45) {
+    // if (window.scrollY > 280) {
     imgImprovise.style.opacity = 1;
-    imgImprovise.style.transform = "translateX(0px)";
+    imgImprovise.style.transform = "none";
   }
 
-  if (window.scrollY <= 1330) {
+  //popup
+
+  if (scrollValue < 0.85 && playOnce) {
+    // if (window.scrollY <= 1330) {
     popup.style.opacity = 0;
     popup.style.transform = "translateX(300px)";
   } else {
     popup.style.opacity = 1;
-    popup.style.transform = "translateX(0px)";
+    popup.style.transform = "none";
+    playOnce = false;
   }
 });
 
 closeBtn.addEventListener("click", () => {
-  popup.style.display = "none";
+  popup.style.opacity = 0;
+  popup.style.transform = "translateX(500px)";
 });
